f6b37b3edd9d1b8a0f5d956da106838324109d3e
Douwe Somers <douwe.somers@student.kuleuven.be>
2022-11-30 17:34:44 +0100

    num capture

---
+++ VGAHexEditorGit.srcs/sources_1/new/num_capture_4bit.v
@@ -0,0 +1,172 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/21/2022 08:56:00 AM
+// Design Name: 
+// Module Name: num_capture_4bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module num_capture_4bit(
+    input wire iClk,iRst,iPush,iStop,
+    output wire[9:0] oAddr,
+    output wire[11:0] oData,
+    
+output wire oWe, 
+    
+    output wire [2:0] oDebug1,oDebug2
+    );
+    //state definitions
+    localparam sRst = 0;
+    localparam sIdle = 1;
+    localparam sPush = 2;
+    localparam sInc = 3;
+    localparam sDsply = 4;
+    
+    reg     [7:0] counter,nCounter;
+   
+    //define internal register
+    reg[2:0] rFSM_Curr,rFSM_Next;
+    reg[3:0] rCurrNum,rNextNum;
+    
+    always @(posedge iClk)
+     begin
+        if(iRst == 1)
+        begin
+        nCounter <= 8'b0;
+        end
+        else
+        begin
+        if(counter < 60)
+        nCounter <= counter +1;
+        else
+        nCounter <= 0;
+        end
+        counter <= nCounter;
+     end
+    
+    //part 1:state register with reset logic
+    always @(posedge iClk)
+     begin
+        if(iRst == 1)
+        begin
+        //#10;
+        rFSM_Curr <= sRst;
+        end
+        else 
+        begin
+        rFSM_Curr <= rFSM_Next; 
+        end
+    end
+    
+    
+    //part 2: next state logic
+    always@(*)
+    begin
+        case(rFSM_Curr)
+            sRst: 
+                rFSM_Next <= sIdle;
+            sIdle: 
+                if(iPush == 0 && iStop == 0)
+                    rFSM_Next <= sIdle;
+                else 
+                    if(iStop == 1)
+                        rFSM_Next <= sDsply;      
+                    else 
+                    if(iPush == 1)
+                        rFSM_Next <= sPush;
+            sPush: 
+                    if(iPush == 0)
+                    rFSM_Next <= sInc;
+                    else                  
+                    rFSM_Next <= sPush;
+                    
+            sInc: 
+                    begin
+                    //#100;
+                    rFSM_Next <= sIdle;
+                    end
+            sDsply:
+                if(iStop == 1)
+                    rFSM_Next <= sDsply;
+                else
+                    rFSM_Next <= sRst;
+            
+            default: 
+            rFSM_Next <= sRst;
+      endcase
+    end
+    
+    //part 3: ouput logic with next number logic integrated
+    reg [3:0] rLEDs;
+    /*
+    always @(*)
+    begin
+    rLEDs = 0;
+      case(rFSM_Curr)
+        sRst: 
+            begin
+             rNextNum = 0;
+             end
+        sIdle:
+            begin 
+            rNextNum = rCurrNum;
+            end
+        sPush:
+            begin 
+            rNextNum = rCurrNum;
+            end
+        sInc:
+            begin 
+            rNextNum = rCurrNum  + 1;
+            end
+        sDsply:
+            begin 
+            rLEDs = rCurrNum;
+            rNextNum = rCurrNum;
+            end
+      default:
+        rNextNum = rNextNum;
+     endcase
+     rCurrNum = rNextNum;
+     end
+     */
+     
+     always @(posedge iClk)
+     begin
+        rCurrNum <= rNextNum;
+    
+     if (rFSM_Curr == sDsply)
+        rLEDs = rCurrNum;
+     else
+        rLEDs = 0;
+     end
+
+
+     always @(*)
+     begin
+        if (rFSM_Curr == sRst)
+            rNextNum = 0;
+        else 
+        if (rFSM_Curr == sInc)
+            rNextNum = rCurrNum + 1;
+        else
+            rNextNum = rCurrNum;
+     end
+        
+     assign oLEDs = rLEDs;
+     endmodule
